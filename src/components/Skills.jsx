d62713adb2bd7c5109c6fba0a3dc6703d145bd51
User <taofiqaiyelabegan45@gmail.com>
2021-12-31 22:24:31 +0800

    new commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -150,7 +150,7 @@ const Main = styled.div`
   background-color: white;
   padding: 3rem 2rem;
   width: 30vw;
-  height: auto;
+  height: 70vh;
   z-index: 3;
   line-height: 1.5rem;
   display: flex;
